d7a5b2ade9544c246257b43d8affa8e05aa23e8f
Cristiano Souza <ltianosouzal@outlook.com>
2025-05-26 22:52:34 -0300

    create CardProduct component

--- frontend/src/components/ContainerMain/index.jsx
+++ frontend/src/components/ContainerMain/index.jsx
@@ -3,7 +3,7 @@ import Header from '../Header';
 
 export default function ContainerMain({ children }) {
   return (
-    <div className="min-h-screen w-full bg-color-secondary-light text-gray-900 dark:text-gray-100 flex flex-col ">
+    <div className="min-h-screen w-full bg-color-secondary-light text-gray-900 dark:text-gray-100 flex flex-col bg-background-color">
       <Header />
       <main className="w-full mx-auto flex-grow flex flex-col items-center justify-center gap-4">
         {children}
